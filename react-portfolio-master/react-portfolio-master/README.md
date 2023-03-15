e64c54236ad43cbda0b0c633e23a2359e06f3142
joelflaco <115979421+joelflaco@users.noreply.github.com>
2023-03-15 20:45:14 +0100

    React Portfolio

---
+++ react-portfolio-master/react-portfolio-master/README.md
@@ -0,0 +1,18 @@
+### Description
+
+A simple portfolio built with React. 
+
+### Setup
+
+Install required dependencies
+
+<pre>yarn install</pre>
+
+
+Start the server
+
+<pre>yarn start</pre>
+
+### More
+
+Modify pages content in  `src/content_option.js`.
---
+++ react-portfolio-master/react-portfolio-master/src/content_option.js
@@ -0,0 +1,140 @@
+const logotext = "Kisame";
+const meta = {
+    title: "Portfolio",
+    description: "I’m Kisame Hoshigaki, member of Akatsuki and I'm from Kirigakure",
+};
+
+const introdata = {
+    title: "I’m Kisame",
+    animated: {
+        first: "I love fighting",
+        second: "I fight cool shinobi",
+        third: "I slice ninjas",
+    },
+    description: "Kisame Hoshigaki (Kisame = Dämonen-Hai; Hoshigaki = getrocknete Dattelpflaume) war ein Mitglied von Akatsuki, das ursprünglich aus dem Dorf Kirigakure stammt. Dort ist er einer der sieben Shinobi-Schwertkämpfer aus Kirigakure gewesen und er führt das mächtigste der sieben Schwerter, das Samehada. Sein Partner war Itachi Uchiha. Im Kampf greift er vor allem auf Suiton-Jutsus und sein immenses Chakra zurück, das sich auf dem Level eines Jinchuuriki befindet, was ihm den Spitznamen: Der schwanzlose Jinchuuriki eingebracht hat. Des Weiteren kann er mit seinem Schwert fusionieren, wobei er eine haiähnliche Form annimmt.",
+    your_img_url: "https://static.wikia.nocookie.net/denaruto3/images/2/25/Kisame.png/revision/latest?cb=20100616173528&path-prefix=de",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Kisames Äußeres ist stark an das eines Hais angelegt, so besitzt er blaue Haut und Haare, Reißzähne, einen haiartigen Kiefer, fischähnliche Augen und kiemenartige Vertiefungen unter den Augen, auch wenn unklar ist, ob es sich hierbei um echte Kiemen handelt, was aufgrund seiner Fusionsfähigkeit mit Samehada aber durchaus möglich ist. Wenn er mit seinem Schwert fusioniert, bekommt er einen langen Schwanz(#nohomo), eine riesige Rückenflosse und Flossen an den Armen. Seine Schultern sind mit Kiemen besetzt, seine Haut wird zu der eines Fisches und kann Chakra aufspüren und er kann sich unter Wasser mit hoher Geschwindigkeit bewegen.",
+};
+const worktimeline = [{
+    jobtitle: "Killer of the week",
+    where: "Konoha",
+    date: "????",
+},
+{
+    jobtitle: "Slicer of the month",
+    where: "Sunagakure",
+    date: "????",
+},
+{
+    jobtitle: "Akatsuki member",
+    where: "????",
+    date: "????",
+},
+];
+
+const skills = [{
+    name: "Ninjutsu",
+    value: 90,
+},
+{
+    name: "Taijutsu",
+    value: 85,
+},
+{
+    name: "Genjutsu",
+    value: 60,
+},
+
+];
+
+const dataportfolio = [{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/800/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/550/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "kisame@kirimail.com",
+    YOUR_FONE: "(040)123-4567",
+    description: "/",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
