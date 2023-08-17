bb485849584ea315d10f53adb09b65c5226dcfde
joelflaco <115979421+joelflaco@users.noreply.github.com>
2023-08-17 14:45:02 +0200

    test_run.py

--- test_run.py
+++ test_run.py
@@ -6,11 +6,11 @@ from run import minus
 class TestAdd(unittest.TestCase):
 
     def test_add_function(self):
-        self.assertEqual(add(1, 2), 3)
-        self.assertEqual(add(4, 7), 11)
+        self.assertEqual(minus(1, 2), 3)
+        self.assertEqual(minus(4, 7), 11)
 
     def test_add_function_with_floats(self):
-        self.assertAlmostEqual(add(2.1, 5.2), 7.3)
+        self.assertAlmostEqual(minus(2.1, 5.2), 7.3)
 
 
 if __name__ == "__main__":
